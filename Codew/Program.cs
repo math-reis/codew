3e6e6fd29f42385917e513d0a78f9c18d2ab28ac
Matheus Reis <math.reis.dev@gmail.com>
2022-06-20 17:35:34 -0300

    Add project files.

---
+++ Codew/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    static void Main(string[] args)
+    {
+        char a = '?';
+        char b = 'b';
+        SameCase(a, b);
+    }
+
+    //Check same case: https://www.codewars.com/kata/5dd462a573ee6d0014ce715b
+    public static int SameCase(char a, char b)
+    {
+        return char.IsLetter(a) & char.IsLetter(b) ? (char.IsUpper(a) == char.IsUpper(b) ? 1 : 0) : -1;
+    }
+}
+    
