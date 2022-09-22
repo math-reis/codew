83d49f1117a0a77671231a435e563b67a66271b4
Matheus Reis <math.reis.dev@gmail.com>
2022-09-22 20:13:46 -0300

    AddBinary.

--- Codew/Program.cs
+++ Codew/Program.cs
@@ -2,13 +2,11 @@
 {
     static void Main(string[] args)
     {
-        long lalala = DigPow(46288, 3);
 
-        Console.WriteLine(lalala);
     }
 
     #region 6 kyu
-    //Playing with digits: https://www.codewars.com/kata/5552101f47fc5178b1000050
+    // Playing with digits: https://www.codewars.com/kata/5552101f47fc5178b1000050
     public static long DigPow(int n, int p)
     {
         double result = 0;
@@ -41,7 +39,7 @@
         return -1;
     }
 
-    //Counting Duplicates: https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1
+    // Counting Duplicates: https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1
     public static int DuplicateCount(string str)
     {
         str = str.ToUpper();
@@ -64,30 +62,33 @@
     #endregion
 
     #region 7 kyu
-    //Binary Addition: https://www.codewars.com/kata/551f37452ff852b7bd000139
-    //public static string AddBinary(int a, int b)
-    //{
-    //    double sum = a + b;
-    //    string binary = "";
-
-    //    return binary;
-    //}
+    // Binary Addition: https://www.codewars.com/kata/551f37452ff852b7bd000139
+    public static string AddBinary(int a, int b)
+    {
+        return Convert.ToString(a + b, 100);
+    }
     #endregion
 
     #region 8 kyu
-    //Opposite number: https://www.codewars.com/kata/56dec885c54a926dcd001095
+    // Convert boolean values to strings 'Yes' or 'No': https://www.codewars.com/kata/53369039d7ab3ac506000467
+    public static string BoolToWord(bool word)
+    {
+        return word ? "Yes" : "No";
+    }
+
+    // Opposite number: https://www.codewars.com/kata/56dec885c54a926dcd001095
     public static int Opposite(int number)
     {
         return number > 0 ? number - (-2 * number) : number + (2 * number);
     }
 
-    //DNA to RNA Conversion: https://www.codewars.com/kata/5556282156230d0e5e000089
+    // DNA to RNA Conversion: https://www.codewars.com/kata/5556282156230d0e5e000089
     public static string DnaToRna(string dna)
     {
         return dna.Replace('T', 'U');
     }
 
-    //Sum Arrays: https://www.codewars.com/kata/53dc54212259ed3d4f00071c
+    // Sum Arrays: https://www.codewars.com/kata/53dc54212259ed3d4f00071c
     public static double SumArray(double[] array)
     {
         double sum = 0;
@@ -104,7 +105,7 @@
         return sum;
     }
 
-    //Cat years, Dog years: https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b
+    // Cat years, Dog years: https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b
     public static int[] humanYearsCatYearsDogYears(int humanYears)
     {
         int catYears = 0;
@@ -132,7 +133,7 @@
         return result;
     }
 
-    //Twice as old: https://www.codewars.com/kata/5b853229cfde412a470000d0
+    // Twice as old: https://www.codewars.com/kata/5b853229cfde412a470000d0
     public static int TwiceAsOld(int dadYears, int sonYears)
     {
         int result = dadYears - (sonYears * 2);
@@ -145,7 +146,7 @@
         return result;
     }
 
-    //Total amount of points: https://www.codewars.com/kata/5bb904724c47249b10000131
+    // Total amount of points: https://www.codewars.com/kata/5bb904724c47249b10000131
     public static int TotalPoints(string[] games)
     {
         int points = 0;
@@ -168,13 +169,13 @@
         return points;
     }
 
-    //Check same case: https://www.codewars.com/kata/5dd462a573ee6d0014ce715b
+    // Check same case: https://www.codewars.com/kata/5dd462a573ee6d0014ce715b
     public static int SameCase(char a, char b)
     {
         return char.IsLetter(a) & char.IsLetter(b) ? (char.IsUpper(a) == char.IsUpper(b) ? 1 : 0) : -1;
     }
 
-    //Multiply: https://www.codewars.com/kata/50654ddff44f800200000004
+    // Multiply: https://www.codewars.com/kata/50654ddff44f800200000004
     public static int Multiply(int a, int b)
     {
         return a * b;
